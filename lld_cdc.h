01b10775503b1ba76e235cce99c5c7e9f83fea8a
NH <skagus@NH>
2024-11-16 20:03:10 +0900

    flash Erase isn't run..

--- lld_cdc.h
+++ lld_cdc.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 #define CDC_DBG		(0)
-#define CDC_TEST	(1)
+#define CDC_DATA	(1)
 
 void cdc_puts(uint32_t itf, char* pBuf);
 void cdc_printf(uint32_t itf,const char* fmt,...);
--- main.c
+++ main.c
@@ -37,7 +37,7 @@
 #include "hardware/irq.h"
 #include "hardware/flash.h"
 #include "hardware/sync.h"
-#include "cli.h"
+#include "lld_cdc.h"
 
 //------------- prototypes -------------//
 
@@ -45,52 +45,45 @@
 #define BYTE_PER_SECTOR		(4096)
 #define PAGE_PER_SECTOR		(BYTE_PER_SECTOR / BYTE_PER_PATE)
 
-void erase_flash(uint32_t offset, uint32_t end_offset)
+void erase_flash(uint32_t offset, uint32_t size)
 {
+	uint32_t end_offset = offset + size;
 	cdc_printf(CDC_DBG, "Erase: %X ~ %X\n", offset, end_offset); 
+
 	while(offset < end_offset)
 	{
 		uint32_t interrupts = save_and_disable_interrupts();
-
 		flash_range_erase(offset, BYTE_PER_SECTOR);
-
 		restore_interrupts(interrupts);
+
 		offset += BYTE_PER_SECTOR;
 	}
 	cdc_printf(CDC_DBG, "Erase done\n");
 }
 
-void prog_flash(uint32_t offset, uint32_t end_offset, uint8_t* data)
+void prog_flash(uint32_t offset, uint32_t size, uint8_t* data)
 {
-	uint32_t data_tmp[BYTE_PER_PAGE / 4];
-	if(NULL == data)
-	{
-		data = (uint8_t*)data_tmp;
-		for(int i=0; i< BYTE_PER_PAGE/4; i++)
-		{
-			data[i] = offset + i;
-		}
-	}
-
+	uint32_t end_offset = offset + size;
 	cdc_printf(CDC_DBG, "PGM: %X ~ %X\n", offset, end_offset); 
 	while(offset < end_offset)
 	{
 		uint32_t interrupts = save_and_disable_interrupts();
-
 		flash_range_program(offset, data, BYTE_PER_PAGE);
-
 		restore_interrupts(interrupts);
+
 		offset += BYTE_PER_PAGE;
+		data += BYTE_PER_PAGE;
 	}
 	cdc_printf(CDC_DBG, "PGM Done\n");
 }
 
-void dump_flash(uint32_t offset, uint32_t end_offset)
+void dump_flash(uint32_t offset, uint32_t size)
 {
-	const uint32_t read_size = 256;
+	const uint32_t read_size = BYTE_PER_PAGE;
 	uint32_t data[read_size / 4];
+	uint32_t end_offset = offset + size;
 
-	cdc_printf(CDC_DBG, "RAM Read: %X %X\n", XIP_BASE + offset, XIP_BASE + end_offset); 
+	cdc_printf(CDC_DBG, "Flash Read: %X %X\n", XIP_BASE + offset, XIP_BASE + end_offset); 
 
 	while(offset < end_offset)
 	{
@@ -111,101 +104,172 @@ void dump_flash(uint32_t offset, uint32_t end_offset)
 	cdc_printf(CDC_DBG, "Read Done\n");
 }
 
-#define BASE_FLASH_FW_COPY		(0x8000)
+#define BASE_FLASH_FW_ORG		(0x200000)  // 2MB.
+#define BASE_FLASH_FW_COPY		(0x300000)  // 3MB.
+
 uint32_t handle_uf2_block(struct uf2_block* uf2)
 {
+	static uint32_t start_addr;
+	cdc_printf(CDC_DBG, "Rcv(%d/%d): %X, %d\n",
+			uf2->block_no, uf2->num_blocks, uf2->target_addr, uf2->payload_size);
+
 	if(0 == uf2->block_no)
 	{
 		uint32_t bytes = uf2->num_blocks * BYTE_PER_PAGE; 
-		erase_flash(BASE_FLASH_FW_COPY, bytes);	
+		start_addr = uf2->target_addr;
+		cdc_printf(CDC_DBG, "erase 0x%X, 0x%X\n", BASE_FLASH_FW_COPY, bytes);
+		erase_flash(BASE_FLASH_FW_COPY, bytes);
+	}
+	cdc_printf(CDC_DBG, "program 0x%X, 0x%X\n",
+		BASE_FLASH_FW_COPY + uf2->target_addr - start_addr,
+		uf2->payload_size);
+	prog_flash(BASE_FLASH_FW_COPY + uf2->target_addr - start_addr, uf2->data, uf2->payload_size);
+	if(uf2->block_no == uf2->num_blocks - 1)
+	{
+		cdc_printf(CDC_DBG, "UF2 Done\n");
 	}
 }
 
+static void uf2_task(void)
+{
+	static int rcv_byte = 0;
+	static uint8_t uf2_buf[512];
+	static struct uf2_block* uf2 = (struct uf2_block*)uf2_buf;
 
+	if ( tud_cdc_n_available(CDC_DATA) )
+	{
+		int avail = 512 - rcv_byte;
+		uint32_t count = tud_cdc_n_read(CDC_DATA, uf2_buf + rcv_byte, avail);
+		rcv_byte += count;
+
+		if(rcv_byte >= 512)
+		{
+			handle_uf2_block(uf2);
+			rcv_byte = 0;
+		}
+	}
+}
 //--------------------------------------------------------------------+
 // USB CDC
 //--------------------------------------------------------------------+
+
+uint32_t parse_number(const char* str)
+{
+	if (str == NULL || *str == '\0')
+	{
+		return 0xFFFFFFFF; // 입력이 NULL이거나 빈 문자열인 경우 0 반환
+	}
+	// 16진수 (0x로 시작) 
+	if (strncmp(str, "0x", 2) == 0 || strncmp(str, "0X", 2) == 0)
+	{
+		return strtol(str, NULL, 16);
+	}
+	// 2진수 (0b로 시작)
+	if (strncmp(str, "0b", 2) == 0 || strncmp(str, "0B", 2) == 0)
+	{
+		return strtol(str + 2, NULL, 2);
+	}
+	// 8진수 (0으로 시작)
+	if (str[0] == '0' && strlen(str) > 1)
+	{
+		return strtol(str + 1, NULL, 8);
+	}
+	// 10진수 (그 외의 경우)
+	return strtol(str, NULL, 10);
+}
+
+#define STR_CMP(name, input)	(0 == strncmp(name, input, strlen(name)))
+
+int gb_RunUF2 = 1;
+
+static void run_cmd(char* line)
+{
+	cdc_printf(CDC_DBG, "CMD: %s\n", line);
+
+	char* tok = strtok(line, " ");
+	cdc_printf(CDC_DBG, "TOK: %s\n", tok);
+	do 
+	{
+		if (STR_CMP("uf2", tok))
+		{
+			gb_RunUF2 = (0 != gb_RunUF2) ? 0 : 1;
+			cdc_printf(CDC_DBG, "UF2 Run: %d\n", gb_RunUF2);
+			return;
+		}
+
+		if (STR_CMP("read", tok))
+		{
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t base = parse_number(tok);
+
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t size = parse_number(tok);
+
+			dump_flash(base, size);
+			return;
+		}
+
+		if (STR_CMP("erase", tok))
+		{
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t base = parse_number(tok);
+
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t size = parse_number(tok);
+
+			erase_flash(base, size);
+			return;
+		}
+
+		if (STR_CMP("prog", tok))
+		{
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t base = parse_number(tok);
+
+			tok = strtok(NULL, " ");
+			if(NULL == tok) break;
+			uint32_t size = parse_number(tok);
+
+
+			prog_flash(base, size, NULL);
+			return;
+		}
+	} while(0);
+
+	cdc_printf(CDC_DBG, "Invalid command\n");
+}
+
 static void cdc_task(void)
 {
-	static uint8_t buf_keep[512];
-	static uint32_t n_valid = 0;
-	static uint8_t cont = 1;
+	static uint32_t len = 0;
+	static uint8_t line[128];
 
-	uint8_t buf[128];
-	
 	if (tud_cdc_n_available(CDC_DBG) )
 	{
+		char buf[64];
 		uint32_t count = tud_cdc_n_read(CDC_DBG, buf, sizeof(buf));
-		if(buf[0] == 'C')
+		
+		strncpy(line + len, buf, count);
+		len += count;
+		buf[count] = '\0';
+		cdc_printf(CDC_DBG, "%s", buf);
+
+		char last = buf[count - 1];
+		if(('\r' == last) || ('\n' == last))
 		{
-			cont = 1;
-			cdc_printf(CDC_DBG, "Cont: %d\n", cont);
-		}
-		else if (buf[0] == 'R')
-		{
-			dump_flash(0xB000, 0xD000);
-		}
-		else if (buf[0] == 'E')
-		{
-			erase_flash(0xC000, 0xD000);
-		}
-		else if (buf[0] == 'P')
-		{
-			prog_flash(0xC000, 0xD000, NULL);
-		}
-		else if (buf[0] == 'D')
-		{
-			cdc_printf(CDC_DBG, "Dump: %d\n", n_valid);
-			for(int i=0; i< n_valid; i++)
+			len--;
+			if(len > 0)
 			{
-				while(tud_cdc_n_write_available(CDC_DBG) < 2)
-				{
-					tud_task();
-				}
-				tud_cdc_n_write_char(CDC_DBG, buf_keep[i]);
-				tud_cdc_n_write_flush(CDC_DBG);
+				line[len] = 0; // remove last.
+				run_cmd(line);
 			}
-			n_valid = 0;
-		}
-		else if(buf[0] == 'X')
-		{
-			cont = 0;
-			cdc_printf(CDC_DBG, "Cont: %d\n", cont);
-		}
-	}
-
-	if(cont == 1)  // RCV has a problem.
-	{
-		static int rcv_byte = 0;
-		static uint8_t uf2_buf[512];
-		static struct uf2_block* uf2 = (struct uf2_block*)uf2_buf;
-
-		if ( tud_cdc_n_available(CDC_TEST) )
-		{
-#if 1
-			int avail = 512 - rcv_byte;
-			uint8_t* ptr = uf2_buf + rcv_byte;
-			uint32_t count = tud_cdc_n_read(CDC_TEST, ptr, avail);
-			rcv_byte += count;
-
-			if(rcv_byte >= 512)
-			{
-				cdc_printf(CDC_DBG, "Rcv(%d/%d): %X, %d\n",
-						uf2->block_no, uf2->num_blocks, uf2->target_addr, uf2->payload_size);
-				handle_uf2_block(uf2);
-				rcv_byte = 0;
-			}
-#else
-			uint32_t count = tud_cdc_n_read(CDC_TEST, buf, sizeof(buf));
-			buf[count] = 0;
-			cdc_printf(CDC_DBG, "RCV: %d\n", count);
-
-			for(int i=0; i< count; i++)
-			{
-				buf_keep[n_valid] = buf[i];
-				n_valid++;
-			}
-#endif
+			len = 0;
+			cdc_printf(CDC_DBG, "$> ");
 		}
 	}
 }
@@ -243,8 +307,13 @@ int main(void)
 			add_alarm_in_ms(2000, alarm_callback, NULL, false);
 		}
 #endif
+//		__wfi();  //__asm ("wfi");
 		tud_task(); // tinyusb device task
 		cdc_task();
+		if(1 == gb_RunUF2)
+		{
+			uf2_task();
+		}
 	}
 
 	return 0;
